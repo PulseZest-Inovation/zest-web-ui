79594649d4aef471c9b2075112fc2399e4f9e24b
Deepanshu9045 <rdeepanshu170@gmail.com>
2026-03-14 12:47:57 +0530

    selector updated

--- src/components/Selector/ZestSelector.tsx
+++ src/components/Selector/ZestSelector.tsx
@@ -9,15 +9,17 @@ export const ZestSelector: React.FC<ZestSelectorProps> = ({
   options,
   placeholder = "Select Option",
   containerClassName = "",
+  value,
   ...props
 }) => (
   <div className={containerClassName}>
     <select
       {...props}
+      value={value}
+      {...(value === undefined && { defaultValue: "" })}
       className={`px-3 py-1.5 border w-80 border-gray-300 rounded-lg text-primary bg-primary focus:outline-none focus:ring-2 focus:ring-blue-400 transition ${
         props.className || ""
       }`}
-      defaultValue=""
     >
       <option value="" disabled>
         {placeholder}
